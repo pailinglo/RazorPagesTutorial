5fe598105efd8e0677f334a72d25cec3ae89906f
pailinglo <pailinglo@gmail.com>
2020-02-22 04:14:25 -0500

    5. Razor Pages Creating Reusable Data Access Project

---
+++ RazorPagesTutorial.Services/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using RazorPagesTutorial.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RazorPagesTutorial.Services
+{
+    //This interface abstraction allows us to use dependency injection.
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAllEmployees();
+    }
+}
