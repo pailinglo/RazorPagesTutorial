885eda66c88a90641471044f78125cc598915929
pailinglo <pailinglo@gmail.com>
2020-02-22 04:42:04 -0500

    6. Registering services in razor pages project

--- RazorPagesTutorial/Pages/Employees/Index.cshtml.cs
+++ RazorPagesTutorial/Pages/Employees/Index.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
 
-namespace RazorPagesTutorial.Pages.Employee
+namespace RazorPagesTutorial.Pages.Employees
 {
     public class IndexModel : PageModel
     {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public IEnumerable<Employee> Employees { get; set; }
+        public IndexModel(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
         public void OnGet()
         {
-
+            Employees = employeeRepository.GetAllEmployees();
         }
     }
 }
--- RazorPagesTutorial/Startup.cs
+++ RazorPagesTutorial/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RazorPagesTutorial.Services;
 
 namespace RazorPagesTutorial
 {
@@ -24,6 +25,8 @@ namespace RazorPagesTutorial
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
