49d7113752cd081df923077639a6f52c045a263f
pailinglo <pailinglo@gmail.com>
2020-02-24 22:24:22 -0500

    14. Editing data in asp net core razor pages

---
+++ RazorPagesTutorial/Pages/Employees/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesTutorial.Models;
+using RazorPagesTutorial.Services;
+
+namespace RazorPagesTutorial.Pages.Employees
+{
+    public class EditModel : PageModel
+    {
+        private readonly IEmployeeRepository employeeRepositoy;
+
+        public EditModel(IEmployeeRepository employeeRepositoy)
+        {
+            this.employeeRepositoy = employeeRepositoy;
+        }
+
+        public Employee Employee { get; set; }
+        public IActionResult OnGet(int id)
+        {
+            Employee = this.employeeRepositoy.GetEmployee(id);
+
+            if(Employee == null)
+            {
+                return RedirectToPage("/NotFound"); 
+            }
+
+
+            return Page();
+        }
+    }
+}
